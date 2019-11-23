522eeb9990a085e1733927831cd9ece4debd830a
Rodel Dagumampan <rdagumampan@gmail.com>
2019-11-23 22:43:46 +0100

    Use .plugins as default plugin folder when no env YUNIQL_PLUGINS

--- yuniql-core/MigrationServiceFactory.cs
+++ yuniql-core/MigrationServiceFactory.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
 using Yuniql.Extensibility;
 using Yuniql.SqlServer;
 
@@ -39,13 +37,16 @@ namespace Yuniql.Core
                 {
                     assemblyFile = Path.Combine(assemblyBasePath, $"Yuniql.{platform}.dll");
                 }
+                else
+                {
+                    assemblyBasePath = Path.Combine(Environment.CurrentDirectory, ".plugins", platform);
+                }
 
                 if (File.Exists(assemblyFile))
                 {
-                    var assemblyContext = new HostAssemblyLoadContext(assemblyBasePath);
+                    var assemblyContext = new PluginAssemblyLoadContext(assemblyBasePath);
                     var assembly = assemblyContext.LoadFromAssemblyPath(assemblyFile);
 
-                    //var assembly = Assembly.LoadFrom(assemblyFile);
                     var sqlDataService = assembly.GetTypes()
                         .Where(t => t.Name.ToLower().Contains($"{platform}dataservice"))
                         .Select(t => Activator.CreateInstance(t, _traceService))
@@ -63,40 +64,10 @@ namespace Yuniql.Core
                 }
                 else
                 {
-                    throw new NotSupportedException($"The target database platform {platform} is not yet supported. See WIKI for supported database platforms and how to configure plugins for non-sqlserver databases.");
+                    throw new NotSupportedException($"The target database platform {platform} is not supported. " +
+                        $"See WIKI for supported database platforms and how to configure plugins for non-sqlserver databases.");
                 }
             }
-        }
-    }
-
-    // This is a collectible (unloadable) AssemblyLoadContext that loads the dependencies
-    // of the plugin from the plugin's binary directory.
-    class HostAssemblyLoadContext : AssemblyLoadContext
-    {
-        // Resolver of the locations of the assemblies that are dependencies of the
-        // main plugin assembly.
-        private AssemblyDependencyResolver _resolver;
-
-        public HostAssemblyLoadContext(string pluginPath) : base(isCollectible: true)
-        {
-            _resolver = new AssemblyDependencyResolver(pluginPath);
-        }
-
-        // The Load method override causes all the dependencies present in the plugin's binary directory to get loaded
-        // into the HostAssemblyLoadContext together with the plugin assembly itself.
-        // NOTE: The Interface assembly must not be present in the plugin's binary directory, otherwise we would
-        // end up with the assembly being loaded twice. Once in the default context and once in the HostAssemblyLoadContext.
-        // The types present on the host and plugin side would then not match even though they would have the same names.
-        protected override Assembly Load(AssemblyName name)
-        {
-            string assemblyPath = _resolver.ResolveAssemblyToPath(name);
-            if (assemblyPath != null)
-            {
-                Console.WriteLine($"Loading assembly {assemblyPath} into the HostAssemblyLoadContext");
-                return LoadFromAssemblyPath(assemblyPath);
-            }
-
-            return null;
         }
     }
 }
---
+++ yuniql-core/PluginAssemblyLoadContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Yuniql.Core
+{
+    //https://github.com/dotnet/samples/tree/master/core/tutorials/Unloading
+    // This is a collectible (unloadable) AssemblyLoadContext that loads the dependencies
+    // of the plugin from the plugin's binary directory.
+    class PluginAssemblyLoadContext : AssemblyLoadContext
+    {
+        // Resolver of the locations of the assemblies that are dependencies of the
+        // main plugin assembly.
+        private AssemblyDependencyResolver _resolver;
+
+        public PluginAssemblyLoadContext(string pluginPath) : base(isCollectible: true)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        // The Load method override causes all the dependencies present in the plugin's binary directory to get loaded
+        // into the HostAssemblyLoadContext together with the plugin assembly itself.
+        // NOTE: The Interface assembly must not be present in the plugin's binary directory, otherwise we would
+        // end up with the assembly being loaded twice. Once in the default context and once in the HostAssemblyLoadContext.
+        // The types present on the host and plugin side would then not match even though they would have the same names.
+        protected override Assembly Load(AssemblyName name)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(name);
+            if (assemblyPath != null)
+            {
+                Console.WriteLine($"Loading assembly {assemblyPath} into the HostAssemblyLoadContext");
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+    }
+}
+
