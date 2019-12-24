c4d9a96a8aa6fde489b356c1a110f5ae3188ad8b
Rodel Dagumampan <rdagumampan@gmail.com>
2019-12-24 03:33:01 +0100

    Not fixed yet: Issue with pgsql dependencies

--- yuniql-core/MigrationServiceFactory.cs
+++ yuniql-core/MigrationServiceFactory.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using Yuniql.Extensibility;
 using Yuniql.SqlServer;
 
 namespace Yuniql.Core
 {
+    //https://docs.microsoft.com/en-us/dotnet/core/dependency-loading/loading-managed
     public class MigrationServiceFactory : IMigrationServiceFactory
     {
         private readonly IEnvironmentService _environmentService;
@@ -19,6 +23,12 @@ namespace Yuniql.Core
             this._traceService = traceService;
         }
 
+        // It is important to mark this method as NoInlining, otherwise the JIT could decide
+        // to inline it into the Main method. That could then prevent successful unloading
+        // of the plugin because some of the MethodInfo / Type / Plugin.Interface / HostAssemblyLoadContext
+        // instances may get lifetime extended beyond the point when the plugin is expected to be
+        // unloaded.
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public IMigrationService Create(string platform)
         {
             _traceService.Debug($"platform: {platform}");
@@ -36,6 +46,9 @@ namespace Yuniql.Core
             }
             {
                 //extracts plugins and creates required services
+                var defaultPluginsBasePath = Path.Combine(Environment.CurrentDirectory, ".plugins");
+                _traceService.Debug($"defaultPluginsBasePath: {defaultPluginsBasePath}");
+
                 var defaultAssemblyBasePath = Path.Combine(Environment.CurrentDirectory, ".plugins", platform);
                 _traceService.Debug($"defaultAssemblyBasePath: {defaultAssemblyBasePath}");
 
@@ -43,28 +56,57 @@ namespace Yuniql.Core
                 _traceService.Debug($"environmentVariableAssemblyBasePath: {environmentVariableAssemblyBasePath}");
 
                 var assemblyBasePath = string.IsNullOrEmpty(environmentVariableAssemblyBasePath) ? defaultAssemblyBasePath : environmentVariableAssemblyBasePath;
+                _traceService.Debug($"assemblyBasePath: {assemblyBasePath}");
+
                 var assemblyFilePath = Path.Combine(assemblyBasePath, $"Yuniql.{platform}.dll");
+                _traceService.Debug($"assemblyFilePath: {assemblyFilePath}");
+
+                //TODO: Use DirectoryService and FileService to find case-insensitive filename
+                var directoryService = new DirectoryService();
+                var fileService = new FileService();
+
+                var directories = directoryService.GetDirectories(defaultPluginsBasePath, "*", SearchOption.AllDirectories).ToList();
+                directories.ForEach(d =>
+                {
+                    _traceService.Debug($"Found plugin dir: {d}");
+
+                    var files = directoryService.GetFiles(d, "*.*").ToList();
+                    files.ForEach(f =>
+                    {
+                        _traceService.Debug($"Found plugin file: {f}. ProductVersion: {FileVersionInfo.GetVersionInfo(f).ProductVersion}, FileVersion: {FileVersionInfo.GetVersionInfo(f).FileVersion}");
+                    });
+                });
 
                 if (File.Exists(assemblyFilePath))
                 {
-                    var assemblyContext = new PluginAssemblyLoadContext(environmentVariableAssemblyBasePath);
+                    // create the unloadable HostAssemblyLoadContext
+                    var assemblyContext = new PluginAssemblyLoadContext(assemblyBasePath);
+
+                    //the plugin assembly into the HostAssemblyLoadContext. 
+                    //the assemblyPath must be an absolute path.
                     var assembly = assemblyContext.LoadFromAssemblyPath(assemblyFilePath);
+                    assemblyContext.Assemblies
+                        .ToList()
+                        .ForEach(a =>
+                        {
+                            _traceService.Debug($"loadedAssembly: {a.FullName}");
+                        });
+                    assemblyContext.Resolving += AssemblyContext_Resolving;
+                    assemblyContext.Unloading += AssemblyContext_Unloading;
 
                     var sqlDataService = assembly.GetTypes()
-                        .Where(t => t.Name.ToLower().Contains($"{platform}dataservice"))
+                        .Where(t => t.Name.ToLower().Contains($"{platform.ToLower()}dataservice"))
                         .Select(t => Activator.CreateInstance(t, _traceService))
                         .Cast<IDataService>()
                         .First();
 
                     var bulkImportService = assembly.GetTypes()
-                        .Where(t => t.Name.ToLower().Contains($"{platform}bulkimportservice"))
+                        .Where(t => t.Name.ToLower().Contains($"{platform.ToLower()}bulkimportservice"))
                         .Select(t => Activator.CreateInstance(t, _traceService))
                         .Cast<IBulkImportService>()
                         .First();
 
                     var tokenReplacementService = new TokenReplacementService(_traceService);
-                    var directoryService = new DirectoryService();
-                    var fileService = new FileService();
 
                     var migrationService = new MigrationService(sqlDataService, bulkImportService, tokenReplacementService, directoryService, fileService, _traceService);
                     return migrationService;
@@ -75,6 +117,26 @@ namespace Yuniql.Core
                         $"See WIKI for supported database platforms and how to configure plugins for non-sqlserver databases.");
                 }
             }
+        }
+
+        private void AssemblyContext_Unloading(System.Runtime.Loader.AssemblyLoadContext obj)
+        {
+            _traceService.Debug($"unloading: {obj.Name}");
+        }
+
+        private System.Reflection.Assembly AssemblyContext_Resolving(System.Runtime.Loader.AssemblyLoadContext assemblyContext, System.Reflection.AssemblyName failedAssembly)
+        {
+            _traceService.Debug($"retryResolving: {failedAssembly.FullName}");
+
+            var defaultAssemblyBasePath = Path.Combine(Environment.CurrentDirectory, ".plugins", "PostgreSql");
+            var assemblyFilePath = Path.Combine(defaultAssemblyBasePath, failedAssembly.Name + ".dll");
+
+            _traceService.Debug($"failedAssemblyFileExists: {File.Exists(assemblyFilePath)}");
+            _traceService.Debug($"attempting to reload {failedAssembly.FullName} from {assemblyFilePath}");
+            assemblyContext.LoadFromAssemblyPath(assemblyFilePath);
+            //assemblyContext.LoadFromAssemblyName(new System.Reflection.AssemblyName(failedAssembly.Name));
+
+            return null;
         }
     }
 }
