1a2dcd8c3f7c8e1f64213688db62814f012c27fd
Rodel Dagumampan <rdagumampan@gmail.com>
2019-12-29 21:37:53 +0100

    Added console-sample

--- sqlserver-samples/console-sample/Program.cs
+++ sqlserver-samples/console-sample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Yuniql.Core;
 
 namespace console_sample
 {
@@ -7,6 +10,37 @@ namespace console_sample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var traceService = new ConsoleTraceService { IsDebugEnabled = true };
+            var configuration = new YuniqlConfiguration
+            {
+                //require configuration
+                WorkspacePath = Path.Combine(Environment.CurrentDirectory, "_db"),
+                ConnectionString = "Server=localhost,1400;Database=yuniqldb;User Id=SA;Password=P@ssw0rd!",
+                AutoCreateDatabase = true,
+
+                //optional configuration for more advanced features
+                TargetVersion = "v1.00",
+                Tokens = new List<KeyValuePair<string, string>> {
+                    new KeyValuePair<string, string>("VwColumnPrefix1","Vw1"),
+                    new KeyValuePair<string, string>("VwColumnPrefix2","Vw2"),
+                    new KeyValuePair<string, string>("VwColumnPrefix3","Vw3"),
+                    new KeyValuePair<string, string>("VwColumnPrefix4","Vw4")
+                },
+                Delimiter = ",",
+                DebugTraceMode = true
+            };
+
+            var migrationServiceFactory = new MigrationServiceFactory(traceService);
+            var migrationService = migrationServiceFactory.Create(configuration.Platform);
+            migrationService.Initialize(configuration.ConnectionString);
+            migrationService.Run(
+                configuration.WorkspacePath,
+                configuration.TargetVersion,
+                configuration.AutoCreateDatabase,
+                configuration.Tokens,
+                configuration.VerifyOnly,
+                configuration.Delimiter);
         }
     }
 }
---
+++ yuniql-core/YuniqlConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Yuniql.Core
+{
+    public class YuniqlConfiguration
+    {
+        public string Platform { get; set; } = "sqlserver";
+
+        public string WorkspacePath { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public bool AutoCreateDatabase { get; set; } = false;
+
+        public string TargetVersion { get; set; }
+
+        public List<KeyValuePair<string, string>> Tokens { get; set; } = new List<KeyValuePair<string, string>>();
+
+        public bool VerifyOnly { get; set; } = false;
+
+        public string Delimiter { get; set; } = ",";
+
+        public bool DebugTraceMode { get; set; } = false;
+    }
+}
