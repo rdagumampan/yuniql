19b3cf8f4e8d62045b9982aa2fe8e334490b614c
Rodel Dagumampan <rdagumampan@gmail.com>
2020-01-11 23:12:37 +0100

    Added --force in -erase CLI command

--- yuniql-cli/EraseOption.cs
+++ yuniql-cli/EraseOption.cs
@@ -7,9 +7,13 @@ namespace Yuniql.CLI
     [Verb("erase", HelpText = "Discover all scripts in the _erase directory and executes them in single transaction")]
     public class EraseOption : BasePlatformOption
     {
-        //yuniql <command> -k "Token1=TokenValue1" -k "Token2=TokenValue2" -k "Token3=TokenValue3" | --token "..." --token "..." --token "..."
-        //yuniql <command> -k "Token1=TokenValue1,Token2=TokenValue2,Token3=TokenValue3" | --token "...,...,..."
+        //yuniql erase -k "Token1=TokenValue1" -k "Token2=TokenValue2" -k "Token3=TokenValue3" | --token "..." --token "..." --token "..."
+        //yuniql erase -k "Token1=TokenValue1,Token2=TokenValue2,Token3=TokenValue3" | --token "...,...,..."
         [Option('k', "token", Required = false, HelpText = "Replace tokens using the passed key-value pairs", Separator = ',')]
         public IEnumerable<string> Tokens { get; set; } = new List<string>();
+
+        //yuniql erase --force 
+        [Option("--force", Required = false, HelpText = "Force execution of erase commands.")]
+        public bool Force { get; set; }
     }
 }
