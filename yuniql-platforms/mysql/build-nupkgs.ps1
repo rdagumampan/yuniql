908f117c038b5c4f414d9e1a615de0fd3bb449a2
Rodel Dagumampan <rdagumampan@gmail.com>
2020-01-23 00:41:40 +0100

    Reorganized platform solution structure

--- yuniql-platforms/mysql/build-nupkgs.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [string]$version,
-    [string]$apiKey
-)
-
-dotnet pack ./src/Yuniql.MySql.csproj -p:packageversion=$version -c release -o nupkgs
-dotnet nuget push ./nupkgs/Yuniql.MySql.$version.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
--- yuniql-platforms/mysql/samples/_post/README.md
+++
@@ -1,2 +0,0 @@
-# The `_post` directory
-Post migration scripts. Executed every time and always the last batch to run.
