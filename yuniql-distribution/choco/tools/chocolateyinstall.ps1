a70e7927f0c24f1cda89e83fe8951bc544c4af6a
Rodel Dagumampan <rdagumampan@gmail.com>
2020-01-27 07:10:21 +0100

    Attemp to fix choco build

--- yuniql-distribution/choco/tools/chocolateyinstall.ps1
+++ yuniql-distribution/choco/tools/chocolateyinstall.ps1
@@ -1,20 +1,31 @@
+
 $packageName = 'yuniql'
 $version = '{version}'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"    
 
+Write-Host "Installing $packageName v$version"
+Write-Host "toolsDir: $toolsDir"
+
 $packageArgs = @{
-    $packageName    = $packageName
-    $url            = "https://github.com/rdagumampan/yuniql/releases/download/latest/yuniql-cli-linux-x64-latest.zip"
-    $unzipLocation  =  $toolsDir
+    packageName    = $packageName
+    url            = 'https://github.com/rdagumampan/yuniql/releases/download/latest/yuniql-cli-win-x64-latest.zip'
+    unzipLocation  =  $toolsDir
 }
 
 Install-ChocolateyZipPackage $packageArgs
 #https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Install-ChocolateyZipPackage.ps1
+#https://github.com/chocolatey/choco/wiki/HelpersInstallChocolateyZipPackage
+
+$installPath = "$toolsDir\$packageName-$version\yuniql.exe"
+Write-Host "installPath: $installPath"
 
 $packageArgs = @{
-    $name   = $packageName
-    $path   = "$toolsDir\$packageName-$version\yuniql.exe"
+    name        = $packageName
+    path        = $installPath
+    checksum    = '9F8011E76CD9BBC9928E27EAD7D22DA24D1D0DC7D060083F49B5166CB583207D'
+    checksumType = 'sha256'
 }
 
 Install-BinFile $packageArgs
 #https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Install-BinFile.ps1
+#https://github.com/chocolatey/choco/wiki/HelpersInstallBinFile
