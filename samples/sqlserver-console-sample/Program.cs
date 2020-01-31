087a4b62fb0ee51a469d7b72f7caf6e5006d36dd
Rodel Dagumampan <rdagumampan@gmail.com>
2020-01-31 19:36:04 +0100

    Updated console sample

--- samples/sqlserver-console-sample/Program.cs
+++ samples/sqlserver-console-sample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+
 using Yuniql.Core;
 
 namespace console_sample
@@ -9,30 +10,17 @@ namespace console_sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            //docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=P@ssw0rd!" -p 1400:1433 -d mcr.microsoft.com/mssql/server:2017-latest
             var traceService = new ConsoleTraceService { IsDebugEnabled = true };
             var configuration = new YuniqlConfiguration
             {
-                //require configuration
                 WorkspacePath = Path.Combine(Environment.CurrentDirectory, "_db"),
                 ConnectionString = "Server=localhost,1400;Database=yuniqldb;User Id=SA;Password=P@ssw0rd!",
-                AutoCreateDatabase = true,
-
-                //optional configuration for more advanced features
-                TargetVersion = "v1.00",
-                Tokens = new List<KeyValuePair<string, string>> {
-                    new KeyValuePair<string, string>("VwColumnPrefix1","Vw1"),
-                    new KeyValuePair<string, string>("VwColumnPrefix2","Vw2"),
-                    new KeyValuePair<string, string>("VwColumnPrefix3","Vw3"),
-                    new KeyValuePair<string, string>("VwColumnPrefix4","Vw4")
-                },
-                Delimiter = ",",
-                DebugTraceMode = true
+                AutoCreateDatabase = true
             };
 
             var migrationServiceFactory = new MigrationServiceFactory(traceService);
-            var migrationService = migrationServiceFactory.Create(configuration.Platform);
+            var migrationService = migrationServiceFactory.Create();
             migrationService.Initialize(configuration.ConnectionString);
             migrationService.Run(
                 configuration.WorkspacePath,
