68bffa11dd25c2932c885a988b56c8a5c887feab
Rodel Dagumampan <rdagumampan@gmail.com>
2020-02-05 23:38:26 +0100

    Added InitalCreate

---
+++ samples/postgresql-efcore-sample/Migrations/20200205223809_InitialCreate.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace efsample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "jobs",
+                columns: table => new
+                {
+                    job_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    job_title = table.Column<string>(maxLength: 35, nullable: false),
+                    min_salary = table.Column<decimal>(type: "numeric(8,2)", nullable: true),
+                    max_salary = table.Column<decimal>(type: "numeric(8,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("jobs_pkey", x => x.job_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "regions",
+                columns: table => new
+                {
+                    region_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    region_name = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("regions_pkey", x => x.region_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "countries",
+                columns: table => new
+                {
+                    country_id = table.Column<string>(fixedLength: true, maxLength: 2, nullable: false),
+                    country_name = table.Column<string>(maxLength: 40, nullable: true),
+                    region_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("countries_pkey", x => x.country_id);
+                    table.ForeignKey(
+                        name: "countries_region_id_fkey",
+                        column: x => x.region_id,
+                        principalTable: "regions",
+                        principalColumn: "region_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "locations",
+                columns: table => new
+                {
+                    location_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    street_address = table.Column<string>(maxLength: 40, nullable: true),
+                    postal_code = table.Column<string>(maxLength: 12, nullable: true),
+                    city = table.Column<string>(maxLength: 30, nullable: false),
+                    state_province = table.Column<string>(maxLength: 25, nullable: true),
+                    country_id = table.Column<string>(fixedLength: true, maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("locations_pkey", x => x.location_id);
+                    table.ForeignKey(
+                        name: "locations_country_id_fkey",
+                        column: x => x.country_id,
+                        principalTable: "countries",
+                        principalColumn: "country_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    department_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    department_name = table.Column<string>(maxLength: 30, nullable: false),
+                    location_id = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("departments_pkey", x => x.department_id);
+                    table.ForeignKey(
+                        name: "departments_location_id_fkey",
+                        column: x => x.location_id,
+                        principalTable: "locations",
+                        principalColumn: "location_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    employee_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(maxLength: 20, nullable: true),
+                    last_name = table.Column<string>(maxLength: 25, nullable: false),
+                    email = table.Column<string>(maxLength: 100, nullable: false),
+                    phone_number = table.Column<string>(maxLength: 20, nullable: true),
+                    hire_date = table.Column<DateTime>(type: "date", nullable: false),
+                    job_id = table.Column<int>(nullable: false),
+                    salary = table.Column<decimal>(type: "numeric(8,2)", nullable: false),
+                    manager_id = table.Column<int>(nullable: true),
+                    department_id = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("employees_pkey", x => x.employee_id);
+                    table.ForeignKey(
+                        name: "employees_department_id_fkey",
+                        column: x => x.department_id,
+                        principalTable: "departments",
+                        principalColumn: "department_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "employees_job_id_fkey",
+                        column: x => x.job_id,
+                        principalTable: "jobs",
+                        principalColumn: "job_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "employees_manager_id_fkey",
+                        column: x => x.manager_id,
+                        principalTable: "employees",
+                        principalColumn: "employee_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "dependents",
+                columns: table => new
+                {
+                    dependent_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(maxLength: 50, nullable: false),
+                    last_name = table.Column<string>(maxLength: 50, nullable: false),
+                    relationship = table.Column<string>(maxLength: 25, nullable: false),
+                    employee_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("dependents_pkey", x => x.dependent_id);
+                    table.ForeignKey(
+                        name: "dependents_employee_id_fkey",
+                        column: x => x.employee_id,
+                        principalTable: "employees",
+                        principalColumn: "employee_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_countries_region_id",
+                table: "countries",
+                column: "region_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_departments_location_id",
+                table: "departments",
+                column: "location_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dependents_employee_id",
+                table: "dependents",
+                column: "employee_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_department_id",
+                table: "employees",
+                column: "department_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_job_id",
+                table: "employees",
+                column: "job_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_manager_id",
+                table: "employees",
+                column: "manager_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_locations_country_id",
+                table: "locations",
+                column: "country_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dependents");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+
+            migrationBuilder.DropTable(
+                name: "jobs");
+
+            migrationBuilder.DropTable(
+                name: "locations");
+
+            migrationBuilder.DropTable(
+                name: "countries");
+
+            migrationBuilder.DropTable(
+                name: "regions");
+        }
+    }
+}
