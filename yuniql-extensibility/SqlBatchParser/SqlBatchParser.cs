a8267232ed97081a29c71167788bf1f1c27654ba
Rodel Dagumampan <rdagumampan@gmail.com>
2020-05-16 15:58:41 +0200

    Investigating broken tests in ci server

--- yuniql-extensibility/SqlBatchParser/SqlBatchParser.cs
+++ yuniql-extensibility/SqlBatchParser/SqlBatchParser.cs
@@ -51,7 +51,7 @@ namespace Yuniql.Extensibility.SqlBatchParser
                     else
                     {
                         //check if current line is within a multi-line comment block
-                        Console.WriteLine($"line: {line}, line.Lenth: {line.Length}, sqlStatementBuilder.Length: {sqlStatementBuilder.Length}, envNewLine.Length: {Environment.NewLine.Length}")
+                        Console.WriteLine($"line: {line}, line.Lenth: {line.Length}, sqlStatementBuilder.Length: {sqlStatementBuilder.Length}, envNewLine.Length: {Environment.NewLine.Length}");
                         Console.WriteLine($"line: {line}, positionStart: {currentPositionNumber + Environment.NewLine.Length}, positionEnd: {currentPositionNumber + sqlStatementBuilder.Length}");
                         if (commentBlocks.Any(c => (currentPositionNumber + Environment.NewLine.Length) >= c.Start 
                             && (currentPositionNumber + sqlStatementBuilder.Length) <= c.End))
