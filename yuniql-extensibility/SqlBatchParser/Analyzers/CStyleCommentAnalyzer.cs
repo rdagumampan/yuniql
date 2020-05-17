24b24423cd0140d09e500df46b2244a33c5030c8
Rodel Dagumampan <rdagumampan@gmail.com>
2020-05-17 13:24:45 +0200

    Testing

--- yuniql-extensibility/SqlBatchParser/Analyzers/CStyleCommentAnalyzer.cs
+++ yuniql-extensibility/SqlBatchParser/Analyzers/CStyleCommentAnalyzer.cs
@@ -7,7 +7,7 @@ namespace Yuniql.Extensibility.SqlBatchParser
 {
     public class CStyleCommentAnalyzer : ICommentAnalyzer
     {
-        public List<CommentAnalyzerResult> Run(string sqlStatementRaw)
+        public List<CommentAnalyzerResult> Run1(string sqlStatementRaw)
         {
             var resultList = new List<CommentAnalyzerResult>();
 
@@ -32,6 +32,43 @@ namespace Yuniql.Extensibility.SqlBatchParser
 
             return resultList;
         }
+
+        public List<CommentAnalyzerResult> Run(string sqlStatementRaw)
+        {
+            var resultList = new List<CommentAnalyzerResult>();
+
+            //single line block comments
+            var singleLineBlockCommentPattern = @"/\*(.*?)\*/?\r?[\n]";
+
+            //inline comments
+            var inlineDashDashCommentPattern = @"--(.*?)\r?\n";
+            var inlineDashDashCommentsOnLastLine = @"--(.*?)$";
+
+            //multi line block comments            
+            var nestedBlockComments = @"/\*
+                                (?>
+                                /\*  (?<LEVEL>)      # On opening push level
+                                | 
+                                \*/ (?<-LEVEL>)     # On closing pop level
+                                |
+                                (?! /\* | \*/ ) . # Match any char unless the opening and closing strings   
+                                )+                         # /* or */ in the lookahead string
+                                (?(LEVEL)(?!))             # If level exists then fail
+                                \*/";
+
+            var regex = new Regex($"{nestedBlockComments}|{singleLineBlockCommentPattern}|{inlineDashDashCommentPattern}|{inlineDashDashCommentsOnLastLine}", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            var match = regex.Match(sqlStatementRaw);
+            while (match.Success)
+            {
+                var commentBlock = new CommentAnalyzerResult { Text = match.Value, Start = match.Index, End = match.Index + match.Length };
+                resultList.Add(commentBlock);
+
+                match = match.NextMatch();
+            }
+
+            return resultList;
+        }
+
     }
 
 }
