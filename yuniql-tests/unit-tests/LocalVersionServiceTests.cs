bbbc5a0448154dff981dc08bec118d2df9016410
Rodel Dagumampan <rdagumampan@gmail.com>
2020-12-30 17:10:32 +0100

    Added new unit test for LocalVersionService

---
+++ yuniql-tests/unit-tests/LocalVersionServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Yuniql.UnitTests
+{
+    //TODO: Implement LocalVersionServiceTests
+    [TestClass]
+    public class LocalVersionServiceTests : TestBase
+    {
+    }
+}
