90db76bddc6216cfdf13d912ba00284b4e287aa7
bymse <egorstrelov@gmail.com>
2020-12-31 15:30:37 +0300

    153 Add .idea to .gitignore + RESERVED_DIRECTORY_NAME class

---
+++ yuniql-core/RESERVED_DIRECTORY_NAME.cs
@@ -0,0 +1,13 @@
+namespace Yuniql.Core
+{
+    public static class RESERVED_DIRECTORY_NAME
+    {
+        public const string INIT = "_init";
+        public const string PRE = "_pre";
+        public const string DRAFT = "_draft";
+        public const string POST = "_post";
+        public const string ERASE = "_erase";
+        public const string DROP = "_drop";
+        public const string TRANSACTION = "_transaction";
+    }
+}
