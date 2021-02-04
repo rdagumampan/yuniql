46b48b6c5ed6fc8b9a686dd951e135de4fcfd805
Rodel Dagumampan <rdagumampan@gmail.com>
2021-02-04 06:11:37 +0100

    Uppdated console sample to v1.1.55 references

--- samples/sqlserver-worker-sample/Program.cs
+++ samples/sqlserver-worker-sample/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Yuniql.AspNetCore;
+using Yuniql.Core;
 
 namespace worker_sample
 {
@@ -14,16 +15,17 @@ namespace worker_sample
             //1. deploy new sql server on docker
             //$ docker run -dit -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=P@ssw0rd!" -p 1400:1433 -d mcr.microsoft.com/mssql/server:2017-latest
 
-            //2. create custom trace message sinks
+            //2. create custom trace message sinks, this can be your own logger framework
             var traceService = new ConsoleTraceService { IsDebugEnabled = true };
 
             CreateHostBuilder(args)
                 //3. run migrations
-                .UseYuniql(traceService, new Configuration
+                .UseYuniql(traceService, new Yuniql.AspNetCore.Configuration
                 {
-                    WorkspacePath = Path.Combine(Environment.CurrentDirectory, "_db"),
-                    ConnectionString = "Server=localhost,1400;Database=yuniqldb;User Id=SA;Password=P@ssw0rd!",
-                    AutoCreateDatabase = true, DebugTraceMode = true
+                    Platform = SUPPORTED_DATABASES.SQLSERVER,
+                    Workspace = Path.Combine(Environment.CurrentDirectory, "_db"),
+                    ConnectionString = "Server=localhost,1400;Database=helloyuniql;User Id=SA;Password=P@ssw0rd!",
+                    IsAutoCreateDatabase = true, IsDebug = true
                 })
                 .Build()
                 .Run();
