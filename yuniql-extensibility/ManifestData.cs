ea4f3e877d0d65edc6e20c0f5b2154753fa47b53
black-eagle17 <deandrenelson731@gmail.com>
2021-08-27 15:22:41 -0400

    add each implementation of ManifestData to supported db platforms

---
+++ yuniql-extensibility/ManifestData.cs
@@ -0,0 +1,15 @@
+
+namespace Yuniql.Extensibility{
+    
+    /// <summary>
+    /// container for supported platform and versions.
+    /// </summary>
+    public class ManifestData
+    {
+        public string Name;
+        public string SupportedVersions;
+        public string Usage; 
+        public string Samples;
+    }
+    
+}
