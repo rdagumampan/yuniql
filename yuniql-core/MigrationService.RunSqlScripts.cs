61f516e495a74b98f4d46ae7b5943db5f9f040e8
Rodel Dagumampan <rdagumampan@gmail.com>
2021-12-19 23:08:17 +0100

    Fixed bug in log messages regarding environment-aware migration issue

--- yuniql-core/MigrationService.RunSqlScripts.cs
+++ yuniql-core/MigrationService.RunSqlScripts.cs
@@ -35,7 +35,7 @@ namespace Yuniql.Core
                 //filter out scripts when environment code is used
                 var sqlScriptFiles = _directoryService.GetFiles(scriptDirectory, "*.sql").ToList();
                 sqlScriptFiles = _directoryService.FilterFiles(workspace, environment, sqlScriptFiles).ToList();
-                _traceService.Info($"Found {sqlScriptFiles.Count} script files on {workspace}" + (sqlScriptFiles.Count > 0 ? Environment.NewLine : string.Empty) +
+                _traceService.Info($"Found {sqlScriptFiles.Count} script files on {scriptDirectory}" + (sqlScriptFiles.Count > 0 ? Environment.NewLine : string.Empty) +
                        $"{string.Join(Environment.NewLine, sqlScriptFiles.Select(s => "  + " + new FileInfo(s).Name))}");
 
                 //execute all script files in the version folder, we also make sure its sorted by file name
@@ -188,7 +188,7 @@ namespace Yuniql.Core
             //extract and filter out scripts when environment code is used
             var bulkFiles = _directoryService.GetFiles(scriptDirectory, "*.csv").ToList();
             bulkFiles = _directoryService.FilterFiles(workspace, environment, bulkFiles).ToList();
-            _traceService.Info($"Found {bulkFiles.Count} script files on {scriptDirectory}" + (bulkFiles.Count > 0 ? Environment.NewLine : string.Empty) +
+            _traceService.Info($"Found {bulkFiles.Count} bulk files on {scriptDirectory}" + (bulkFiles.Count > 0 ? Environment.NewLine : string.Empty) +
                    $"{string.Join(Environment.NewLine, bulkFiles.Select(s => "  + " + new FileInfo(s).Name))}");
             bulkFiles.Sort();
             bulkFiles.ForEach(csvFile =>
