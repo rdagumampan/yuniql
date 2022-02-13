f992f17401579c902de6173678c3d94a2982913a
Rodel Dagumampan <rdagumampan@gmail.com>
2022-02-13 22:23:00 +0100

    Move PRed oracle parser into its own class for better testing

---
+++ yuniql-platforms/oracle/OracleConnectionStringParser.cs
@@ -0,0 +1,61 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Yuniql.Extensibility;
+
+namespace Yuniql.Oracle
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OracleConnectionStringParser
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OracleConnectionStringParser()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_connectionString"></param>
+        /// <returns></returns>
+        public ConnectionInfo Parse(string _connectionString)
+        {
+            //Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=49161))(CONNECT_DATA=(SERVICE_NAME=xe)));User Id=myuser;Password=mypassword;
+            var stringParts = _connectionString.Split('(');
+
+            if (_connectionString.Contains("DATA SOURCE", StringComparison.OrdinalIgnoreCase))
+            {
+                var match = Regex.Match(_connectionString, "DATA SOURCE=(\\w|:|/)*", RegexOptions.IgnoreCase);
+                var source = match.Value.Split('=')[1];
+                var connectionData = source.Split(':', '/');
+                var host = connectionData[0];
+                var port = source.Contains(":") ? int.Parse(connectionData[1]) : 1521;
+                var serviceName = source.Contains("/") ? connectionData.Last() : string.Empty;
+                return new ConnectionInfo { DataSource = $"{host}", Port = port, Database = serviceName };
+            }
+            else
+            {
+                //HOST=localhost)
+                var hostPair = stringParts.First(s => s.Contains("HOST", StringComparison.InvariantCultureIgnoreCase)).Split("=");
+                var host = hostPair[1].Substring(0, hostPair[1].IndexOf(")"));
+
+                //PORT=49161)
+                var portPair = stringParts.First(s => s.Contains("PORT", StringComparison.InvariantCultureIgnoreCase)).Split("=");
+                var port = Convert.ToInt32(portPair[1].Substring(0, portPair[1].IndexOf(")")).Trim());
+
+                //SERVICE_NAME=xe)
+                var serviceNamePair = stringParts.First(s => s.Contains("SERVICE_NAME", StringComparison.InvariantCultureIgnoreCase)).Split("=");
+                var serviceName = serviceNamePair[1].Substring(0, serviceNamePair[1].IndexOf(")"));
+
+                var connectionStringBuilder = new OracleConnectionStringBuilder(_connectionString);
+                return new ConnectionInfo { DataSource = $"{host}", Port = port, Database = serviceName };
+            }
+
+        }
+    }
+}
---
+++ yuniql-platforms/oracle/OracleSqlBatchParser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Yuniql.Extensibility.SqlBatchParser;
+
+namespace Yuniql.Oracle
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OracleSqlBatchParser : ISqlBatchParser
+    {
+        private const string CustomStatementStartRegEx = "create( )*(or( )*replace )?( )*(procedure|package|trigger)";
+        private const string CustomStatementEndRegEx = ";(\n|\r| )*/( )*$";
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sqlStatementRaw"></param>
+        /// <returns></returns>
+        public List<SqlStatement> Parse(string sqlStatementRaw)
+        {
+            return Parse(sqlStatementRaw, new SqlBatchParserOption());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sqlStatementRaw"></param>
+        /// <param name="parseOption"></param>
+        /// <returns></returns>
+        public List<SqlStatement> Parse(string sqlStatementRaw, SqlBatchParserOption parseOption)
+        {
+            var results = new List<SqlStatement>();
+            var sqlStatement = string.Empty;
+            var sqlStatementLine2 = string.Empty; byte lineNo = 0;
+            bool isCustomStatement = false;
+
+            using (var sr = new StringReader(sqlStatementRaw))
+            {
+                while ((sqlStatementLine2 = sr.ReadLine()) != null)
+                {
+                    if (sqlStatementLine2.Length > 0 && !sqlStatementLine2.StartsWith("--"))
+                    {
+                        if (!isCustomStatement && Regex.IsMatch(sqlStatementLine2, CustomStatementStartRegEx, RegexOptions.IgnoreCase))
+                        {
+                            isCustomStatement = true;
+                        }
+
+                        sqlStatement += (sqlStatement.Length > 0 ? Environment.NewLine : string.Empty) + sqlStatementLine2;
+
+                        if (IsCommandEnded(sqlStatement, isCustomStatement))
+                        {
+                            results.Add(new SqlStatement { BatchText = RemoveEndCommand(sqlStatement, isCustomStatement) });
+                            isCustomStatement = false;
+                            sqlStatement = string.Empty;
+                        }
+                    }
+                    ++lineNo;
+                }
+
+                //pickup the last formed sql statement
+                if (!string.IsNullOrEmpty(sqlStatement.Trim()))
+                {
+                    results.Add(new SqlStatement { BatchText = sqlStatement });
+                }
+            }
+
+            return results;
+        }
+
+        private string RemoveEndCommand(string sqlStatement, bool isCustomStatement)
+        {
+            if (isCustomStatement)
+            {
+                return sqlStatement.TrimEnd('\n', '\r', ' ', '/');
+            }
+            else
+            {
+                return sqlStatement.TrimEnd('\n', '\r', ' ', ';');
+            }
+        }
+
+        private bool IsCommandEnded(string sqlStatement, bool isCustomStatement)
+        {
+            if (isCustomStatement)
+            {
+                return Regex.IsMatch(sqlStatement, CustomStatementEndRegEx);
+            }
+            else
+            {
+                return sqlStatement.TrimEnd('\n', '\r', ' ').EndsWith(";");
+            }
+        }
+
+    }
+}
