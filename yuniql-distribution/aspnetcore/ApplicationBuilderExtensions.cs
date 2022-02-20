dc262a9c872f04d1e468f284e956ca2866b2a90b
Rodel Dagumampan <rdagumampan@gmail.com>
2022-02-21 00:20:30 +0100

    Updated AspNetCore project with v1.2.25 references

--- yuniql-distribution/aspnetcore/ApplicationBuilderExtensions.cs
+++ yuniql-distribution/aspnetcore/ApplicationBuilderExtensions.cs
@@ -7,43 +7,6 @@ namespace Yuniql.AspNetCore
     public static class ApplicationBuilderExtensions
     {
         /// <summary>
-        /// Runs database migrations with Yuniql. This uses default trace service FileTraceService.
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="configuration">Desired configuration when yuniql runs. Set your workspace location, connection string, target version and other parameters.</param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseYuniql(
-            this IApplicationBuilder builder,
-            Configuration configuration
-        )
-        {
-            var traceService = new FileTraceService { IsDebugEnabled = configuration.IsDebug };
-            return builder.UseYuniql(traceService, configuration);
-        }
-
-        /// <summary>
-        /// Runs database migrations with Yuniql. This uses your specific implementation of ITraceService interface.
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="traceService">Your custom implementation of ITraceService interface</param>
-        /// <param name="configuration">Desired configuration when yuniql runs. Set your workspace location, connection string, target version and other parameters.</param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseYuniql(
-            this IApplicationBuilder builder,
-            ITraceService traceService,
-            Configuration configuration
-        )
-        {
-            ConfigurationHelper.Initialize(configuration);
-
-            var migrationServiceFactory = new MigrationServiceFactory(traceService);
-            var migrationService = migrationServiceFactory.Create();
-            migrationService.Run();
-
-            return builder;
-        }
-
-        /// <summary>
         /// Runs database migrations with Yuniql. Use this interface to run migrations targeting non-sqlserver platforms such as PostgreSql and MySql.
         /// </summary>
         /// <param name="builder"></param>
