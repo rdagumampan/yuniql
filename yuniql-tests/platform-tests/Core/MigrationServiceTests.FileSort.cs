be80e7ced2f99e9594a639ac0de2d5c5f19ec77c
Rodel Dagumampan <rdagumampan@gmail.com>
2022-03-06 08:07:56 +0100

    Added test coverage for erase scripts

--- yuniql-tests/platform-tests/Core/MigrationServiceTests.FileSort.cs
+++ yuniql-tests/platform-tests/Core/MigrationServiceTests.FileSort.cs
@@ -200,5 +200,100 @@ namespace Yuniql.PlatformTests.Core
             _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_4).ShouldBeTrue();
         }
 
+
+        [TestMethod]
+        public void Test_Run_With_Sort_Order_Manifest_Sub_Directories()
+        {
+            //arrange
+            var directoryService = new DirectoryService();
+            var fileService = new FileService();
+            var workspaceService = new WorkspaceService(_traceService, directoryService, fileService);
+
+            workspaceService.Init(_testConfiguration.WorkspacePath);
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "test_v0_00_01.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_1));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "test_v0_00_02.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_2));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "test_v0_00_03.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_3));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "test_v0_00_04.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_4));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "test_v0_00_05.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_5));
+
+            string childDirectory = Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "level1");
+            Directory.CreateDirectory(childDirectory);
+            _testDataService.CreateScriptFile(Path.Combine(childDirectory, $"test_v0_00_07.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_7));
+            _testDataService.CreateScriptFile(Path.Combine(childDirectory, $"test_v0_00_08.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_8));
+            _testDataService.CreateScriptFile(Path.Combine(childDirectory, $"test_v0_00_09.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_9));
+
+            var sortOrderFile = $"test_v0_00_03.sql{Environment.NewLine}" +
+                                $"test_v0_00_01.sql{Environment.NewLine}" +
+                                $"test_v0_00_05.sql{Environment.NewLine}" +
+                                $"test_v0_00_02.sql{Environment.NewLine}" +
+                                $"test_v0_00_04.sql{Environment.NewLine}" +
+                                $"test_v0_00_09.sql{Environment.NewLine}" +
+                                $"test_v0_00_08.sql{Environment.NewLine}" +
+                                $"test_v0_00_07.sql";
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", "_manifest.ini"), sortOrderFile);
+
+            //act
+            var configuration = _testConfiguration.GetFreshConfiguration();
+            var migrationService = _migrationServiceFactory.Create(configuration.Platform);
+            migrationService.Run();
+
+            //assert
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_3).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_1).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_5).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_2).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_4).ShouldBeTrue();
+
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_9).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_8).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_7).ShouldBeTrue();
+        }
+
+
+        [TestMethod]
+        public void Test_Run_With_Sort_Order_Manifest_Erase()
+        {
+            //arrange
+            var directoryService = new DirectoryService();
+            var fileService = new FileService();
+            var workspaceService = new WorkspaceService(_traceService, directoryService, fileService);
+
+            workspaceService.Init(_testConfiguration.WorkspacePath);
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", $"test_v0_00_01.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_1));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", $"test_v0_00_02.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_2));
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "v0.00", $"test_v0_00_03.sql"), _testDataService.GetSqlForCreateDbObject(TEST_DBOBJECTS.DB_OBJECT_3));
+
+            //act
+            var configuration = _testConfiguration.GetFreshConfiguration();
+            var migrationService = _migrationServiceFactory.Create(configuration.Platform);
+            migrationService.Run();
+
+            //assert
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_1).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_2).ShouldBeTrue();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_3).ShouldBeTrue();
+
+            //arrange
+            _testDataService.CreateScriptFile(Path.Combine(Path.Combine(_testConfiguration.WorkspacePath, RESERVED_DIRECTORY_NAME.ERASE), $"erase1.sql"), _testDataService.GetSqlForEraseDbObjects());
+            _testDataService.CreateScriptFile(Path.Combine(Path.Combine(_testConfiguration.WorkspacePath, RESERVED_DIRECTORY_NAME.ERASE), $"erase2.sql"), _testDataService.GetSqlForEraseDbObjects());
+            _testDataService.CreateScriptFile(Path.Combine(Path.Combine(_testConfiguration.WorkspacePath, RESERVED_DIRECTORY_NAME.ERASE), $"erase3.sql"), _testDataService.GetSqlForEraseDbObjects());
+            _testDataService.CreateScriptFile(Path.Combine(Path.Combine(_testConfiguration.WorkspacePath, RESERVED_DIRECTORY_NAME.ERASE), $"erase4.sql"), _testDataService.GetSqlForEraseDbObjects());
+            _testDataService.CreateScriptFile(Path.Combine(Path.Combine(_testConfiguration.WorkspacePath, RESERVED_DIRECTORY_NAME.ERASE), $"erase5.sql"), _testDataService.GetSqlForEraseDbObjects());
+
+            var sortOrderFile = $"erase3.sql{Environment.NewLine}" +
+                    $"erase1.sql{Environment.NewLine}" +
+                    $"erase5.sql{Environment.NewLine}" +
+                    $"erase2.sql{Environment.NewLine}" +
+                    $"erase4.sql";
+            _testDataService.CreateScriptFile(Path.Combine(_testConfiguration.WorkspacePath, "_erase", "_manifest.ini"), sortOrderFile);
+
+            //act
+            migrationService.Erase();
+
+            //assert
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_1).ShouldBeFalse();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_2).ShouldBeFalse();
+            _testDataService.CheckIfDbObjectExist(_testConfiguration.ConnectionString, TEST_DBOBJECTS.DB_OBJECT_3).ShouldBeFalse();
+        }
     }
 }
